fd601f4207821b8087cbc720cfeb01daf54c7afb
Shabbir Suterwala <shabbirsuterwala@yahoo.com>
2014-10-26 13:14:38 -0700

    Added links to help

--- ui.R
+++ ui.R
@@ -4,7 +4,8 @@ shinyUI(fluidPage(
     verticalLayout(
         fluidRow(
             column(width=10, titlePanel("NCBI Gene Database")),
-            column(width=2, a("Help", href="./help.html"))
+            column(width=1, a("Help", href="ftp://ftp.ncbi.nlm.nih.gov/gene/README")),
+            column(width=1, a("Pitch", href="http://shabss.github.io/NCBIGeneDBBrowserPitch/index.html"))
         ),
         htmlOutput("selectUI"),
         mainPanel(
